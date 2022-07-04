1051be3d97fcd28b20b8c33b1684f211dc02fd7a
PC_01 <PC_01@DESKTOP-8UHGQL9>
2022-07-04 13:26:30 +0500

    Add project files.

---
+++ testwithGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testwithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
